39e7219125477a09b82d89f71d53ecebfbbfa006
Schwynn Sant <sschwynn@gmail.com>
2024-03-25 14:12:07 +0700

    dentist ja

--- src/app/booking/page.tsx
+++ src/app/booking/page.tsx
@@ -1,7 +1,11 @@
+import LocationDateReserve from "@/components/LocationDateReserve";
+
 export default function Booking() {
   return (
     <main>
       <h1 className="text-center p-5 mt-[50px]">Booking Page</h1>
+
+      <LocationDateReserve />
     </main>
   );
 }
